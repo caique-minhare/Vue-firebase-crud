58e95573eabca4a0f77bf0921a25839ff2f56e57
Caique Minhare <caique.minhare.eng@gmail.com>
2018-07-09 13:18:07 -0400

    Criando formulario para adição de item

--- src/components/adicionarItem.vue
+++ src/components/adicionarItem.vue
@@ -1,5 +1,22 @@
 <template lang="html">
-  <div class="">
+  <div class="container">
+    <div class="card">
+      <div class="card-header">
+        <h3>Adicionar Item</h3>
+      </div>
+
+      <div class="card-body">
+        <form v-on:submit.prevent="adicionarItem">
+          <div class="form-group">
+            <label>Nome:</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Adicionar item">
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
