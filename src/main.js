bc392ca56084e08dbb5597a6914ceab27391f70a
Caique Minhare <caique.minhare.eng@gmail.com>
2018-07-09 08:22:17 -0400

    Adicionando rotas ao projeto

--- src/main.js
+++ src/main.js
@@ -1,9 +1,43 @@
 import Vue from 'vue'
 import VueFire from 'vuefire'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+
+import adicionarItem from './components/adicionarItem.vue'
+import editarItem from './components/editarItem.vue'
+import listarItem from './components/listarItem.vue'
+import Home from './components/Home.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueFire)
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    name: 'Home',
+    path: '/',
+    component: Home
+  },
+  {
+        name: 'Adicionar',
+        path: '/adicionar',
+        component: adicionarItem
+  },
+  {
+      name: 'Editar',
+      path: '/editar/:id',
+      component: editarItem
+  },
+  {
+      name: 'Listar',
+      path: '/index',
+      component: listarItem
+  },
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes });
+
 
 new Vue({
   render: h => h(App)
